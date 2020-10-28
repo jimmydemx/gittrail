0a830cb02c171ea5d71a68527e652b826790dbc5
jimmy demx <jmxdemx@gmail.com>
2020-10-28 21:27:50 +0100

    2nd

--- src/main.js
+++ src/main.js
@@ -3,8 +3,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import './assets/style/reset.css'
+import './assets/style/border.css' //解决移动端高分辨率屏幕显示border:1px 实际先是2px
+import fastClick from 'fastclick' // 使用npm install fastclick --save 引用的依赖，解决移动端点击事件延迟300s执行的问题
 
 Vue.config.productionTip = false
+fastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
@@ -13,3 +17,4 @@ new Vue({
   components: { App },
   template: '<App/>'
 })
+
